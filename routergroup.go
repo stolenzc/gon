ed106ef403065bf085000bf467863a0ac1d187aa
zhouc <1022590068@qq.com>
2025-07-02 23:36:10 +0800

    fix(routergroup): 修复注册路由调用Handler改为handler

--- routergroup.go
+++ routergroup.go
@@ -2,7 +2,11 @@ package gon
 
 import (
 	"net/http"
+	"regexp"
 )
+
+// regEnLetter 用于匹配请求方式是否都是大写字母，使用 regexp.MustCompile 编译一次后可以复用，以此提高性能
+var regEnLetter = regexp.MustCompile(`^[A-Z]*$`)
 
 // anyMethods 定义了 Engine 默认支持的9颗路由树的路由方法名
 var anyMethods = []string{
@@ -22,16 +26,16 @@ type IRouter interface {
 type IRoutes interface {
 	Use(...HandlerFunc) IRoutes // 注册路由中间件（处理函数链）
 
-	Handler(string, string, HandlerFunc) IRoutes // 传入请求方式和路径进行路由注册
-	Any(string, HandlerFunc) IRoutes             // 注册任意请求方式的路由处理函数
-	GET(string, HandlerFunc) IRoutes             // 注册 GET 请求的路由处理函数
-	POST(string, HandlerFunc) IRoutes            // 注册 POST 请求的路由处理函数
-	PUT(string, HandlerFunc) IRoutes             // 注册 PUT 请求的路由处理函数
-	DELETE(string, HandlerFunc) IRoutes          // 注册 DELETE 请求的路由处理函数
-	HEAD(string, HandlerFunc) IRoutes            // 注册 HEAD 请求的路由处理函数
-	PATCH(string, HandlerFunc) IRoutes           // 注册 PATCH 请求的路由处理函数
-	OPTIONS(string, HandlerFunc) IRoutes         // 注册 OPTIONS 请求的路由处理函数
-	MATCH([]string, string, HandlerFunc) IRoutes // 注册多种请求方式的路由处理函数
+	Handler(string, string, ...HandlerFunc) IRoutes // 传入请求方式和路径进行路由注册
+	Any(string, ...HandlerFunc) IRoutes             // 注册任意请求方式的路由处理函数
+	GET(string, ...HandlerFunc) IRoutes             // 注册 GET 请求的路由处理函数
+	POST(string, ...HandlerFunc) IRoutes            // 注册 POST 请求的路由处理函数
+	PUT(string, ...HandlerFunc) IRoutes             // 注册 PUT 请求的路由处理函数
+	DELETE(string, ...HandlerFunc) IRoutes          // 注册 DELETE 请求的路由处理函数
+	HEAD(string, ...HandlerFunc) IRoutes            // 注册 HEAD 请求的路由处理函数
+	PATCH(string, ...HandlerFunc) IRoutes           // 注册 PATCH 请求的路由处理函数
+	OPTIONS(string, ...HandlerFunc) IRoutes         // 注册 OPTIONS 请求的路由处理函数
+	MATCH([]string, string, ...HandlerFunc) IRoutes // 注册多种请求方式的路由处理函数
 
 	// TODO 后续实现静态文件解析方法
 	// StaticFile(string, string) IRoutes                    // 注册静态文件路由
@@ -69,53 +73,63 @@ func (group *RouterGroup) Group(relativePath string, handlers ...HandlerFunc) *R
 
 }
 
-// Handler 真实实现路由注册的逻辑，后续 GET、POST 等方法会调用该方法进行注册
-func (group *RouterGroup) Handler(method string, path string, handler HandlerFunc) IRoutes {
-	// TODO 这里需要实现路由注册逻辑
+// handler 真实实现路由注册的逻辑，后续 GET、POST 等方法会调用该方法进行注册
+func (group *RouterGroup) handler(httpMethod, relativePath string, handlers HandlerChain) IRoutes{
+	absolutePath := group.calculateAbsolutePath(relativePath)
+	handlers = group.combineHandlers(handlers)
+	group.engine.addRoute(httpMethod, absolutePath, handlers)
 	return group.returnObj()
 }
 
-func (group *RouterGroup) GET(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodGet, path, handler)
+
+func (group *RouterGroup) Handler(method string, path string, handlers ...HandlerFunc) IRoutes {
+	if matched := regEnLetter.MatchString(method); !matched {
+		panic("http method " + method + " is not valid")
+	}
+	return group.handler(method, path, handlers)
 }
 
-func (group *RouterGroup) POST(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodPost, path, handler)
+func (group *RouterGroup) GET(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodGet, path, handlers)
 }
 
-func (group *RouterGroup) PUT(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodPut, path, handler)
+func (group *RouterGroup) POST(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodPost, path, handlers)
 }
 
-func (group *RouterGroup) DELETE(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodDelete, path, handler)
+func (group *RouterGroup) PUT(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodPut, path, handlers)
 }
 
-func (group *RouterGroup) HEAD(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodHead, path, handler)
+func (group *RouterGroup) DELETE(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodDelete, path, handlers)
 }
 
-func (group *RouterGroup) PATCH(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodPatch, path, handler)
+func (group *RouterGroup) HEAD(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodHead, path, handlers)
 }
 
-func (group *RouterGroup) OPTIONS(path string, handler HandlerFunc) IRoutes {
-	return group.Handler(http.MethodOptions, path, handler)
+func (group *RouterGroup) PATCH(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodPatch, path, handlers)
+}
+
+func (group *RouterGroup) OPTIONS(path string, handlers ...HandlerFunc) IRoutes {
+	return group.handler(http.MethodOptions, path, handlers)
 }
 
 // Any 会将该方法和 path 注册到所有支持的 HTTP 方法上
 // GET、POST、PUT、DELETE、HEAD、PATCH、OPTIONS、TRACE、CONNECT
-func (group *RouterGroup) Any(path string, handler HandlerFunc) IRoutes {
+func (group *RouterGroup) Any(path string, handlers ...HandlerFunc) IRoutes {
 	for _, method := range anyMethods {
-		group.Handler(method, path, handler)
+		group.handler(method, path, handlers)
 	}
 	return group.returnObj()
 }
 
 // MATCH 会将该方法和 path 注册到指定的 HTTP 方法上
-func (group *RouterGroup) MATCH(methods []string, path string, handler HandlerFunc) IRoutes {
+func (group *RouterGroup) MATCH(methods []string, path string, handlers ...HandlerFunc) IRoutes {
 	for _, method := range methods {
-		group.Handler(method, path, handler)
+		group.handler(method, path, handlers)
 	}
 	return group.returnObj()
 }
